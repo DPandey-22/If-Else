07b8955d20c59b9d6657120aaf226c6c7b2493f5
Dular Pandey <129974442+DPandey-22@users.noreply.github.com>
2023-04-05 20:40:38 +0530

    Add files via upload

---
+++  low no Q.no.3.py
@@ -0,0 +1,8 @@
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+if num1<num2:   
+    number="the lowest number"
+else:   
+    number="not lowest number"    
+print("the first number is",number)
+
